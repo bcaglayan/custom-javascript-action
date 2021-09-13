645d3d08f9663b693dc8aa142b7e88fcd2a52180
batuhan <batuhancaglayan1@gmail.com>
2021-09-13 12:10:34 +0300

    abc6

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 try {
 
+    const command = core.getInput('command')
     core.exportVariable('NODE_OPTIONS', '-r ./abc');
     // export NODE_OPTIONS='-r epsagon-frameworks'
     // `who-to-greet` input defined in action metadata file
@@ -11,8 +13,13 @@ try {
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
+
+    if (command) {
+        core.info(`[Thundra] Executing the command`)
+        await exec.exec(`sh -c "${command}"`)
+    }
 } catch (error) {
     core.setFailed(error.message);
 }

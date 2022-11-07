76aad32e5a7056217266f0b8cbff77d46f266ac2
bcaglayan <batuhan@thundra.io>
2022-11-07 13:53:14 +0300

    abcd

--- index.js
+++ index.js
@@ -2,65 +2,20 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
-const JEST_DEFAULT_ARGUMENTS = [
-    '--testRunner=jest-circus/runner',
-    '--env=@thundra/core/dist/ForesightBootstrap.js'
-    // '--env=ttt/test-environment.js'
-];
-
-// --testRunner=jest-circus/runner --env=@thundra/core/dist/ForesightBootstrap.js
-
 async function run() {
     try {
         
-        const apikey = core.getInput('apikey')
-        const project_id = core.getInput('project_id');
+        const inputTryer = core.getInput('tags_input_tryer');
 
-        if (!apikey) {
-            core.warning('Thundra API Key is not present. Exiting early...')
-            core.warning('Instrumentation failed.')
-        
-            process.exit(core.ExitCode.Success)
-        }
-        
-        if (!project_id) {
-            core.warning('Thundra Project ID is not present. Exiting early...')
-            core.warning('Instrumentation failed.')
-        
-            process.exit(core.ExitCode.Success)
-        }
+        console.log('inputTryer', inputTryer);
+        core.exportVariable('inputTryer', inputTryer);
 
-        core.exportVariable('THUNDRA_APIKEY', apikey)
-        core.exportVariable('THUNDRA_AGENT_TEST_PROJECT_ID', project_id)
+        const aaa = core.getMultilineInput('tags_*', {
+            required: false
+        })
 
-        const command = core.getInput('command');
-        const autoTestRun = core.getInput('autoTestRun');
-        const loadPredefiendModule = core.getInput('loadPredefiendModule');
-       
-        // export NODE_OPTIONS='-r epsagon-frameworks'
-        // `who-to-greet` input defined in action metadata file
-        const nameToGreet = core.getInput('who-to-greet');
-        console.log(`Hello ${nameToGreet}!`);
-        const time = (new Date()).toTimeString();
-        core.setOutput("time", time);
-        // Get the JSON webhook payload for the event that triggered the workflow
-        // const payload = JSON.stringify(github.context.payload, undefined, 2)
-        // console.log(`The event payload: ${payload}`);
-        
-        if (loadPredefiendModule) {
-            console.log('loadPredefiendModule loadPredefiendModule loadPredefiendModule');
-            core.exportVariable('NODE_OPTIONS', '-r @thundra/core');
-        }
-
-        if (command) {
-            core.info(`[Thundra] Executing the command`)
-            await exec.exec(`sh -c "${command}"`)
-        }
-        
-        if (autoTestRun) {
-            await exec.exec('npm test', ['--', ...JEST_DEFAULT_ARGUMENTS])
-            // await exec.exec(`sh -c "jest --testRunner=jest-circus/runner --env=ttt/test-environment.js"`)
-        }
+        console.log('aaa', aaa);
+        core.exportVariable('aaa', aaa);
     } catch (error) {
         core.setFailed(error.message);
     }
